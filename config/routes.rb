637f192a5b1563de3d193a0603f5ba9144c1f563
Nbuck3905 <Nathanbuchanan671@gmail.com>
2019-11-25 18:12:34 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   resources :ducks, only: [:index]
   resources :students, only: [:index]
+
+  #get('/students',{to: 'students#index'})
+  
+  get('students/new', {to: 'students#new'})
+  
+  get('students/:id', {to: 'students#show'})
+  
+  post('students/new', {to: 'students#new'})
+
+  get('students/:id/edit', {to: 'students#edit'})
+
+  post('students/:id/edit', {to: 'students#edit'})
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
+Student.destroy_all
+Duck.destroy_all
+
+student_ids = []
 10.times do
-  Student.create(name: Faker::Name.name, mod: rand(1..5))
+  student = Student.create(name: Faker::Name.name, mod: rand(1..5))
+  student_ids.push(student.id)
 end
 
 5.times do
-  Duck.create(name: Faker::Dog.meme_phrase, description: Faker::Dog.breed)
+  Duck.create(name: Faker::Dog.meme_phrase, description: Faker::Dog.breed, student_id: student_ids.sample())
 end
